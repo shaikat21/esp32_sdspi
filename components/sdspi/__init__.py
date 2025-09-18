6caede821cff0b5beeac96a71d84b5284191dd98
shaikat21 <shaikat.ee@gmail.com>
2025-09-18 01:59:07 -0400

    Update __init__.py

--- components/sdspi/__init__.py
+++ components/sdspi/__init__.py
@@ -1,5 +1,5 @@
-import esphome.codegen as cg
 import esphome.config_validation as cv
+import esphome.codegen as cg
 from esphome.const import CONF_ID
 
 sdspi_ns = cg.esphome_ns.namespace("sdspi")
@@ -10,15 +10,13 @@ CONF_SCLK = "sclk_pin"
 CONF_MOSI = "mosi_pin"
 CONF_MISO = "miso_pin"
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(SDSPICard),
-        cv.Required(CONF_CS): cv.pin_schema(),
-        cv.Required(CONF_SCLK): cv.pin_schema(),
-        cv.Required(CONF_MOSI): cv.pin_schema(),
-        cv.Required(CONF_MISO): cv.pin_schema(),
-    }
-).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(SDSPICard),
+    cv.Required(CONF_CS): cv.pin_schema(),
+    cv.Required(CONF_SCLK): cv.pin_schema(),
+    cv.Required(CONF_MOSI): cv.pin_schema(),
+    cv.Required(CONF_MISO): cv.pin_schema(),
+}).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     cs = await cg.gpio_pin_expression(config[CONF_CS])
