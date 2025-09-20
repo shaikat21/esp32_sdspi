535c88df6ba2f3166e07e8205af1a347c372bb06
shaikat21 <shaikat.ee@gmail.com>
2025-09-19 23:21:46 -0400

    Update __init__.py

--- components/sdspi_csv/__init__.py
+++ components/sdspi_csv/__init__.py
@@ -1,31 +1,36 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import pins
 from esphome.components import spi
-from esphome.const import CONF_ID, CONF_CS_PIN
+from esphome.const import CONF_ID, CONF_CS_PIN, CONF_MAX_FILES, CONF_MOUNT_POINT
 
-CODEOWNERS = ["@shaikat21"]
 DEPENDENCIES = ["spi"]
 
-sdspi_ns = cg.esphome_ns.namespace("sdspi")
-SDSPIComponent = sdspi_ns.class_("SDSPIComponent", cg.Component, spi.SPIDevice)
+sdspi_csv_ns = cg.esphome_ns.namespace("sdspi_csv")
+SDSpiCSV = sdspi_csv_ns.class_("SDSpiCSV", cg.Component, spi.SPIDevice)
 
-CONF_MOUNT_POINT = "mount_point"
+CONF_CS_PIN = "cs_pin"
 CONF_MAX_FILES = "max_files"
+CONF_MOUNT_POINT = "mount_point"
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(SDSPIComponent),
-    cv.Required(CONF_CS_PIN): pins.gpio_output_pin_schema,
-    cv.Optional(CONF_MOUNT_POINT, default="/sdcard"): cv.string,
-    cv.Optional(CONF_MAX_FILES, default=5): cv.int_range(min=1, max=10),
-}).extend(spi.spi_device_schema(cs_pin_required=False))
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(SDSpiCSV),
+            cv.Required(CONF_CS_PIN): cv.gpio_pin_schema(),
+            cv.Optional(CONF_MAX_FILES, default=10): cv.int_,
+            cv.Optional(CONF_MOUNT_POINT, default="/sdcard"): cv.string,
+        }
+    )
+    .extend(spi.SPI_DEVICE_SCHEMA)   # << needed to get `spi_id` and reuse existing SPI bus 
+    .extend(cv.COMPONENT_SCHEMA)
+)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await spi.register_spi_device(var, config)
-    
-    cs_pin = await cg.gpio_pin_expression(config[CONF_CS_PIN])
-    cg.add(var.set_cs_pin(cs_pin))
-    cg.add(var.set_mount_point(config[CONF_MOUNT_POINT]))
+
+    cs = await cg.gpio_pin_expression(config[CONF_CS_PIN])
+    cg.add(var.set_cs_pin(cs))
     cg.add(var.set_max_files(config[CONF_MAX_FILES]))
+    cg.add(var.set_mount_point(config[CONF_MOUNT_POINT]))
