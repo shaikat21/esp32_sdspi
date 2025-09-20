58e18da622284444501704ce41f1ede878ce9cbc
shaikat21 <shaikat.ee@gmail.com>
2025-09-20 02:51:38 -0400

    Create sd_spi_card.h

---
+++ components/sd_spi_card/sd_spi_card.h
@@ -0,0 +1,204 @@
+#pragma once
+
+#include "esphome/core/gpio.h"
+#include "esphome/core/defines.h"
+#include "esphome/core/component.h"
+#include "esphome/core/automation.h"
+
+#ifdef USE_SENSOR
+#include "esphome/components/sensor/sensor.h"
+#endif
+#ifdef USE_TEXT_SENSOR
+#include "esphome/components/text_sensor/text_sensor.h"
+#endif
+
+#ifdef USE_ESP_IDF
+#include "driver/sdspi_host.h"
+#include "driver/spi_common.h"
+#include "sdmmc_cmd.h"
+#endif
+
+namespace esphome {
+namespace sd_spi_card {
+
+enum MemoryUnits : short { Byte = 0, KiloByte = 1, MegaByte = 2, GigaByte = 3, TeraByte = 4, PetaByte = 5 };
+
+#ifdef USE_SENSOR
+struct FileSizeSensor {
+  sensor::Sensor *sensor{nullptr};
+  std::string path;
+
+  FileSizeSensor() = default;
+  FileSizeSensor(sensor::Sensor *, std::string const &path);
+};
+#endif
+
+struct FileInfo {
+  std::string path;
+  size_t size;
+  bool is_directory;
+
+  FileInfo(std::string const &, size_t, bool);
+};
+
+class SdSpi : public Component {
+#ifdef USE_SENSOR
+  SUB_SENSOR(used_space)
+  SUB_SENSOR(total_space)
+  SUB_SENSOR(free_space)
+#endif
+#ifdef USE_TEXT_SENSOR
+  SUB_TEXT_SENSOR(sd_card_type)
+#endif
+ public:
+  enum ErrorCode {
+    ERR_PIN_SETUP,
+    ERR_MOUNT,
+    ERR_NO_CARD,
+  };
+
+  void setup() override;
+  void loop() override;
+  void dump_config() override;
+
+  void write_file(const char *path, const uint8_t *buffer, size_t len, const char *mode);
+  void write_file(const char *path, const uint8_t *buffer, size_t len);
+  void append_file(const char *path, const uint8_t *buffer, size_t len);
+  bool delete_file(const char *path);
+  bool delete_file(std::string const &path);
+  bool create_directory(const char *path);
+  bool remove_directory(const char *path);
+  std::vector<uint8_t> read_file(char const *path);
+  std::vector<uint8_t> read_file(std::string const &path);
+  bool is_directory(const char *path);
+  bool is_directory(std::string const &path);
+  std::vector<std::string> list_directory(const char *path, uint8_t depth);
+  std::vector<std::string> list_directory(std::string path, uint8_t depth);
+  std::vector<FileInfo> list_directory_file_info(const char *path, uint8_t depth);
+  std::vector<FileInfo> list_directory_file_info(std::string path, uint8_t depth);
+  size_t file_size(const char *path);
+  size_t file_size(std::string const &path);
+
+#ifdef USE_SENSOR
+  void add_file_size_sensor(sensor::Sensor *, std::string const &path);
+#endif
+
+  void set_miso_pin(uint8_t);
+  void set_mosi_pin(uint8_t);
+  void set_clk_pin(uint8_t);
+  void set_cs_pin(uint8_t);
+  void set_power_ctrl_pin(GPIOPin *);
+
+ protected:
+  ErrorCode init_error_;
+  uint8_t miso_pin_;
+  uint8_t mosi_pin_;
+  uint8_t clk_pin_;
+  uint8_t cs_pin_;
+  GPIOPin *power_ctrl_pin_{nullptr};
+
+#ifdef USE_ESP_IDF
+  sdmmc_card_t *card_;
+  sdspi_host_t host_;
+  spi_bus_config_t bus_cfg_;
+#endif
+
+#ifdef USE_SENSOR
+  std::vector<FileSizeSensor> file_size_sensors_{};
+#endif
+
+  void update_sensors();
+#ifdef USE_ESP32_FRAMEWORK_ARDUINO
+  std::string sd_card_type_to_string(int) const;
+#endif
+#ifdef USE_ESP_IDF
+  std::string sd_card_type() const;
+#endif
+
+  std::vector<FileInfo> &list_directory_file_info_rec(const char *path, uint8_t depth, std::vector<FileInfo> &list);
+  static std::string error_code_to_string(ErrorCode);
+};
+
+// Action templates
+template<typename... Ts> class SdSpiWriteFileAction : public Action<Ts...> {
+ public:
+  SdSpiWriteFileAction(SdSpi *parent) : parent_(parent) {}
+  TEMPLATABLE_VALUE(std::string, path)
+  TEMPLATABLE_VALUE(std::vector<uint8_t>, data)
+
+  void play(Ts... x) {
+    auto path = this->path_.value(x...);
+    auto buffer = this->data_.value(x...);
+    this->parent_->write_file(path.c_str(), buffer.data(), buffer.size());
+  }
+
+ protected:
+  SdSpi *parent_;
+};
+
+template<typename... Ts> class SdSpiAppendFileAction : public Action<Ts...> {
+ public:
+  SdSpiAppendFileAction(SdSpi *parent) : parent_(parent) {}
+  TEMPLATABLE_VALUE(std::string, path)
+  TEMPLATABLE_VALUE(std::vector<uint8_t>, data)
+
+  void play(Ts... x) {
+    auto path = this->path_.value(x...);
+    auto buffer = this->data_.value(x...);
+    this->parent_->append_file(path.c_str(), buffer.data(), buffer.size());
+  }
+
+ protected:
+  SdSpi *parent_;
+};
+
+template<typename... Ts> class SdSpiCreateDirectoryAction : public Action<Ts...> {
+ public:
+  SdSpiCreateDirectoryAction(SdSpi *parent) : parent_(parent) {}
+  TEMPLATABLE_VALUE(std::string, path)
+
+  void play(Ts... x) {
+    auto path = this->path_.value(x...);
+    this->parent_->create_directory(path.c_str());
+  }
+
+ protected:
+  SdSpi *parent_;
+};
+
+template<typename... Ts> class SdSpiRemoveDirectoryAction : public Action<Ts...> {
+ public:
+  SdSpiRemoveDirectoryAction(SdSpi *parent) : parent_(parent) {}
+  TEMPLATABLE_VALUE(std::string, path)
+
+  void play(Ts... x) {
+    auto path = this->path_.value(x...);
+    this->parent_->remove_directory(path.c_str());
+  }
+
+ protected:
+  SdSpi *parent_;
+};
+
+template<typename... Ts> class SdSpiDeleteFileAction : public Action<Ts...> {
+ public:
+  SdSpiDeleteFileAction(SdSpi *parent) : parent_(parent) {}
+  TEMPLATABLE_VALUE(std::string, path)
+
+  void play(Ts... x) {
+    auto path = this->path_.value(x...);
+    this->parent_->delete_file(path.c_str());
+  }
+
+ protected:
+  SdSpi *parent_;
+};
+
+// Utility functions
+long double convertBytes(uint64_t, MemoryUnits);
+std::string memory_unit_to_string(MemoryUnits);
+MemoryUnits memory_unit_from_size(size_t);
+std::string format_size(size_t);
+
+}  // namespace sd_spi_card
+}  // namespace esphome
